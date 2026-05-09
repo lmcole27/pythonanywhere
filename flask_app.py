f80f19f922887e71cb8478437e6e704609af6f66
Linda Cole <lmcole27@yahoo.ca>
2026-05-09 12:42:41 +0000

    commented out log and weather tracker projects

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
 import logging
 import sys
-from flask import Flask, render_template, url_for, request, redirect, flash, send_file, Response, stream_with_context, jsonify, session
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TelField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, request, Response, stream_with_context, jsonify, session #redirect, flash, send_file, url_for
+#from flask_wtf import FlaskForm
+#from wtforms import StringField, SubmitField, TelField
+#from wtforms.validators import DataRequired
 #from flask_wtf.csrf import CSRFProtect
-from twilio.rest import Client
+# from twilio.rest import Client
 import requests
 from openai import OpenAI
 from flask_cors import CORS
@@ -16,7 +16,6 @@ import uuid
 #import atexit
 # from apscheduler.schedulers.background import BackgroundScheduler #This is not allowed in pythonanywhere... need to find another method.
 import os
-import csv
 import datetime
 from dotenv import load_dotenv
 
@@ -34,14 +33,14 @@ load_dotenv(os.path.join(project_folder, '.env'))
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 
-#RAIN TRACKER SECRET ENDPOINT INFORMTION - WEATHER & TWILIO
-ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
-AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
-wds_auth = os.environ.get('WDS_AUTH')
-from_tel = os.environ.get('from_tel')
+# #RAIN TRACKER SECRET ENDPOINT INFORMTION - WEATHER & TWILIO
+# ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
+# AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+# wds_auth = os.environ.get('WDS_AUTH')
+# from_tel = os.environ.get('from_tel')
 
 #CREATE API CLIENTS
-twilioClient = Client(ACCOUNT_SID, AUTH_TOKEN)
+# twilioClient = Client(ACCOUNT_SID, AUTH_TOKEN)
 openaiClient = OpenAI(api_key=os.environ['OPENAI_API_KEY'], organization=os.environ['ORGANIZATION'], project=os.environ['PROJECT'])
 
 #CREATE WEBAPP
@@ -52,12 +51,12 @@ app.config['SESSION_COOKIE_SECURE'] = True  # Set to True in production
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
-#RAIN TRACKER INPUT FORM
-class rainForm(FlaskForm):
-    city = StringField('City', validators=[DataRequired()])
-    country = StringField('Country', validators=[DataRequired()])
-    phone_no = TelField('Phone Number', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+# #RAIN TRACKER INPUT FORM
+# class rainForm(FlaskForm):
+#     city = StringField('City', validators=[DataRequired()])
+#     country = StringField('Country', validators=[DataRequired()])
+#     phone_no = TelField('Phone Number', validators=[DataRequired()])
+#     submit = SubmitField('Submit')
 
 #OPENAI CLIENT FUNCTION
 def generate_response(question: str, chat_history):
@@ -103,146 +102,146 @@ def welcome():
     send_telegram_alert("Homepage")
     return render_template("index.html")
 
-#LOG FILE PROCESSOR HOMEPAGE
-@app.route('/log', methods=['GET', 'POST'])
-def process_log():
-    return render_template("processlog.html")
+# #LOG FILE PROCESSOR HOMEPAGE
+# @app.route('/log', methods=['GET', 'POST'])
+# def process_log():
+#     return render_template("processlog.html")
 
-#LOG FILE PROCESSOR OUTPUT
-@app.route('/upload', methods=['POST'])
-def upload():
-    # Check if a file was uploaded
-    if 'file' not in request.files:
-        return render_template("processlog.html")
-    file = request.files['file']
+# #LOG FILE PROCESSOR OUTPUT
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     # Check if a file was uploaded
+#     if 'file' not in request.files:
+#         return render_template("processlog.html")
+#     file = request.files['file']
 
-    # Check if the file has a valid name
-    if file.filename == '':
-        return render_template("processlog.html")
+#     # Check if the file has a valid name
+#     if file.filename == '':
+#         return render_template("processlog.html")
 
-    #Set variables
-    i_list = [] # list of Unique IDs
-    count = 0 # Number of Unique IDs
-    writeunid = 0 # Indicator of whether this is an error to capture
+#     #Set variables
+#     i_list = [] # list of Unique IDs
+#     count = 0 # Number of Unique IDs
+#     writeunid = 0 # Indicator of whether this is an error to capture
 
-    # Read file content directly / load to memory
-    file_content = file.read().decode('utf-8')  # Assuming the file is a text file
-    lines=file_content.splitlines()
+#     # Read file content directly / load to memory
+#     file_content = file.read().decode('utf-8')  # Assuming the file is a text file
+#     lines=file_content.splitlines()
 
-    for line in lines:
-        if 'severity' in line:
-            if 'severity="Critical"' in line:
-                writeunid = 1
-            else:
-                writeunid =0
-        if "<unid>" in line and writeunid==1:
-            unid= str(line).strip()
-            i_list.append(unid)
-            writeunid==0
+#     for line in lines:
+#         if 'severity' in line:
+#             if 'severity="Critical"' in line:
+#                 writeunid = 1
+#             else:
+#                 writeunid =0
+#         if "<unid>" in line and writeunid==1:
+#             unid= str(line).strip()
+#             i_list.append(unid)
+#             writeunid==0
 
-    for item in i_list:
-        count += 1
+#     for item in i_list:
+#         count += 1
 
-    return render_template("processlog_output.html", DATA=i_list, count=count)
+#     return render_template("processlog_output.html", DATA=i_list, count=count)
 
-#LOG FILE PROCESSOR HOMEPAGE
-@app.route('/compare', methods=['GET', 'POST'])
-def comparefiles():
-    return render_template("compare.html")
+# #LOG FILE PROCESSOR HOMEPAGE
+# @app.route('/compare', methods=['GET', 'POST'])
+# def comparefiles():
+#     return render_template("compare.html")
 
-#LOG FILE PROCESSOR OUTPUT
-@app.route('/uploadfiles', methods=['POST'])
-def uploadfiles():
-    # Check if a file was uploaded
-    if 'file1' not in request.files:
-        return render_template("compare.html")
-    elif 'file2' not in request.files:
-        return render_template("compare.html")
+# #LOG FILE PROCESSOR OUTPUT
+# @app.route('/uploadfiles', methods=['POST'])
+# def uploadfiles():
+#     # Check if a file was uploaded
+#     if 'file1' not in request.files:
+#         return render_template("compare.html")
+#     elif 'file2' not in request.files:
+#         return render_template("compare.html")
 
-    primaryfile = request.files['file1']
-    secondaryfile = request.files['file2']
+#     primaryfile = request.files['file1']
+#     secondaryfile = request.files['file2']
 
-    # Check if the file has a valid name
-    if primaryfile.filename == '':
-        return render_template("compare.html")
-    elif secondaryfile.filename == '':
-        return render_template("compare.html")
+#     # Check if the file has a valid name
+#     if primaryfile.filename == '':
+#         return render_template("compare.html")
+#     elif secondaryfile.filename == '':
+#         return render_template("compare.html")
 
-    # Read file content directly
-    primaryfile_content = primaryfile.read().decode('utf-8')  # Assuming the file is a text file
-    secondaryfile_content = secondaryfile.read().decode('utf-8')
-    primelines=primaryfile_content.splitlines()
-    secondlines=secondaryfile_content.splitlines()
+#     # Read file content directly
+#     primaryfile_content = primaryfile.read().decode('utf-8')  # Assuming the file is a text file
+#     secondaryfile_content = secondaryfile.read().decode('utf-8')
+#     primelines=primaryfile_content.splitlines()
+#     secondlines=secondaryfile_content.splitlines()
 
-        # Initialize an empty set to store the rows
-    primeset = set()
-    secondset = set()
-    missingitemset = set()
-    extraitemset = set()
+#         # Initialize an empty set to store the rows
+#     primeset = set()
+#     secondset = set()
+#     missingitemset = set()
+#     extraitemset = set()
 
-    # Initialize count variables
-    primecount = 0
-    secondcount = 0
-    blanks = 0
-    missingcount = 0
-    blanks2 = 0
-    duplicates2 = 0
-    extracount = 0
+#     # Initialize count variables
+#     primecount = 0
+#     secondcount = 0
+#     blanks = 0
+#     missingcount = 0
+#     blanks2 = 0
+#     duplicates2 = 0
+#     extracount = 0
 
-    # Append the row to the list
-    for row in primelines:
-        if row == "":
-            blanks += 1
-        else:
-            primeset.add(row)
+#     # Append the row to the list
+#     for row in primelines:
+#         if row == "":
+#             blanks += 1
+#         else:
+#             primeset.add(row)
 
-    primecount = len(primelines)
-    duplicates = int(primecount) - int(len(primeset)) - int(blanks)
+#     primecount = len(primelines)
+#     duplicates = int(primecount) - int(len(primeset)) - int(blanks)
 
-    for row in secondlines:
-        if row == "":
-            blanks2 += 1
-        else:
-            secondset.add(row)
+#     for row in secondlines:
+#         if row == "":
+#             blanks2 += 1
+#         else:
+#             secondset.add(row)
 
-    secondcount = len(secondlines)
-    duplicates2 = int(secondcount) - int(len(secondset)) - int(blanks2)
+#     secondcount = len(secondlines)
+#     duplicates2 = int(secondcount) - int(len(secondset)) - int(blanks2)
 
-    for item in primeset:
-        if item not in secondset:
-            missingitemset.add(item)
-            missingcount+=1
+#     for item in primeset:
+#         if item not in secondset:
+#             missingitemset.add(item)
+#             missingcount+=1
 
-    for item in secondset:
-        if item not in primeset:
-            extraitemset.add(item)
-            extracount += 1
+#     for item in secondset:
+#         if item not in primeset:
+#             extraitemset.add(item)
+#             extracount += 1
 
-    # Save to a CSV file
-    with open("output.csv", mode="w", newline="") as csvoutputfile:
-        writer = csv.writer(csvoutputfile)
-        writer.writerow(missingitemset)
+#     # Save to a CSV file
+#     with open("output.csv", mode="w", newline="") as csvoutputfile:
+#         writer = csv.writer(csvoutputfile)
+#         writer.writerow(missingitemset)
 
-    with open('output.txt', 'w') as txtoutputfile:
-        for row in missingitemset:
-            txtoutputfile.write(row + '\n')
+#     with open('output.txt', 'w') as txtoutputfile:
+#         for row in missingitemset:
+#             txtoutputfile.write(row + '\n')
 
-    return render_template("compare_output.html", missingitemlist=missingitemset, primecount=primecount, secondcount=secondcount, blanks=blanks, duplicates=duplicates, missingcount=missingcount, blanks2=blanks2, extracount=extracount, extraitemlist=extraitemset, duplicates2=duplicates2)
+#     return render_template("compare_output.html", missingitemlist=missingitemset, primecount=primecount, secondcount=secondcount, blanks=blanks, duplicates=duplicates, missingcount=missingcount, blanks2=blanks2, extracount=extracount, extraitemlist=extraitemset, duplicates2=duplicates2)
 
 
-@app.route('/downloads')
-def download():
-    return render_template('downloads.html')
+# @app.route('/downloads')
+# def download():
+#     return render_template('downloads.html')
 
-@app.route('/download/<filename>')
-def download_file(filename):
-    file_path = filename
+# @app.route('/download/<filename>')
+# def download_file(filename):
+#     file_path = filename
 
-    try:
-        # Serve the file for download
-        return send_file(file_path, as_attachment=True)
-    except Exception as e:
-        return f"Error: {str(e)}", 404
+#     try:
+#         # Serve the file for download
+#         return send_file(file_path, as_attachment=True)
+#     except Exception as e:
+#         return f"Error: {str(e)}", 404
 
 
 #FLASK DAD JOKES
@@ -260,63 +259,63 @@ def flask_jokes():
 def js_jokes():
     return render_template("js_jokes.html")
 
-#UMBRELLA APP HOME PAGE
-@app.route('/rain', methods=['GET', 'POST'])
-def rain():
-    form = rainForm()
+# #UMBRELLA APP HOME PAGE
+# @app.route('/rain', methods=['GET', 'POST'])
+# def rain():
+#     form = rainForm()
 
-    if request.method == "POST":
+#     if request.method == "POST":
 
-        #CONVERT TO LOWERCASE
-        location = (str(request.form["city"]) + "," + str(request.form["country"])).lower()
-        to_tel = request.form["phone_no"]
+#         #CONVERT TO LOWERCASE
+#         location = (str(request.form["city"]) + "," + str(request.form["country"])).lower()
+#         to_tel = request.form["phone_no"]
 
-        #BUILD THE ENDPOINT
-        WDS_ENDPOINT = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations="+ location + "&aggregateHours=24&forcastDays=1&unitGroup=us&shortColumnNames=true&contentType=json&key=" + wds_auth
+#         #BUILD THE ENDPOINT
+#         WDS_ENDPOINT = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations="+ location + "&aggregateHours=24&forcastDays=1&unitGroup=us&shortColumnNames=true&contentType=json&key=" + wds_auth
 
-        #FIND THE WEATHER
-        response = requests.get(url=WDS_ENDPOINT)
+#         #FIND THE WEATHER
+#         response = requests.get(url=WDS_ENDPOINT)
 
-        try:
-            data = response.json()
-            precipitation = data['locations'][location]['values'][0]['pop']
+#         try:
+#             data = response.json()
+#             precipitation = data['locations'][location]['values'][0]['pop']
 
-        except:
-            flash("Hmmm... we can't find that city. Please try again.")
-            return redirect(url_for('rain'))
+#         except:
+#             flash("Hmmm... we can't find that city. Please try again.")
+#             return redirect(url_for('rain'))
 
-        else:
-            #LOGIC FOR RAIN/UMBRELLA
-            if precipitation >50:
-                content = "Bring an Umbrella in " + request.form["city"] + "!"
-            else:
-                content = "No rain today in " + request.form["city"] + "!"
+#         else:
+#             #LOGIC FOR RAIN/UMBRELLA
+#             if precipitation >50:
+#                 content = "Bring an Umbrella in " + request.form["city"] + "!"
+#             else:
+#                 content = "No rain today in " + request.form["city"] + "!"
 
-            try:
-                #SEND THE NOTIFICATION TO YOUR DEVICE USING TWILIO
-                message = twilioClient.messages \
-                                .create(
-                                        body=content,
-                                        from_=from_tel,
-                                        to=to_tel
-                                    )
+#             try:
+#                 #SEND THE NOTIFICATION TO YOUR DEVICE USING TWILIO
+#                 message = twilioClient.messages \
+#                                 .create(
+#                                         body=content,
+#                                         from_=from_tel,
+#                                         to=to_tel
+#                                     )
 
-            except:
-                flash("Hmmm... we can't reach that telephone number. Please try again.")
-                return redirect(url_for('rain'))
+#             except:
+#                 flash("Hmmm... we can't reach that telephone number. Please try again.")
+#                 return redirect(url_for('rain'))
 
-            else:
-                flash("Sent! Check your messages.")
+#             else:
+#                 flash("Sent! Check your messages.")
 
-            finally:
-                return redirect(url_for('rain'))
+#             finally:
+#                 return redirect(url_for('rain'))
 
-        finally:
-            return redirect(url_for('rain'))
-    else:
-        return render_template("rain.html", form=form)
+#         finally:
+#             return redirect(url_for('rain'))
+#     else:
+#         return render_template("rain.html", form=form)
 
-    return render_template("rain.html", form=form)
+#     return render_template("rain.html", form=form)
 
 # ASSISTANT CODE
 
